dd58cabc8929a6a4681c86eb2598f3f2f8af878f
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-05-07 20:26:03 +0100

    Initialising repository

---
+++ DFWin/DFWin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DFWin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
