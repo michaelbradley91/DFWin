aab628904681394a0f07b9b7f9c1f162543a26cd
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-05-14 23:11:35 +0100

    Fiddling around with ico appearance...

--- DFWin/DFWin/Properties/AssemblyInfo.cs
+++ DFWin/DFWin/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DFWin")]
-[assembly: AssemblyProduct("DFWin")]
+[assembly: AssemblyTitle("Dwarf Fortress")]
+[assembly: AssemblyProduct("Dwarf Fortress")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
