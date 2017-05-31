9360cb6378d92f6ca80cad0af61e97676b6e3fa3
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-05-31 13:53:46 +0200

    Fixing exit middleware

--- DFWin/DFWin.Core/Middleware/ExitMiddleware.cs
+++ DFWin/DFWin.Core/Middleware/ExitMiddleware.cs
@@ -8,14 +8,9 @@ namespace DFWin.Core.Middleware
     {
         public GameState Update(GameState previousState, Func<GameState, GameState> next)
         {
-            return next(UpdateWithExit(previousState));
-        }
-
-        private static GameState UpdateWithExit(GameState previousState)
-        {
-            return previousState.GameInput.UserInput.KeyboardInput.KeyboardState.IsKeyDown(Keys.Escape) ?
-                new GameState(previousState.ScreenState, previousState.GameInput, previousState.FrameRate, true) :
-                previousState;
+            return previousState.GameInput.UserInput.KeyboardInput.KeyboardState.IsKeyDown(Keys.Escape)
+                ? new GameState(previousState.ScreenState, previousState.GameInput, previousState.FrameRate, true)
+                : next(previousState);
         }
     }
 }
