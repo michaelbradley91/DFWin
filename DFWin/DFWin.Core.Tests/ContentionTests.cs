8e2d7c79eec1fa5dcd89793a1864d3b14b49964e
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-06-02 18:30:28 +0200

    Fixing lots of configuration issues + established convention tests.

--- DFWin/DFWin.Core.Tests/ContentionTests.cs
+++ DFWin/DFWin.Core.Tests/ContentionTests.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Bogus;
+using DFWin.Core.Helpers;
+using DFWin.Core.Inputs.DwarfFortress;
 using DFWin.Core.States;
+using DFWin.Core.Tests.TestSetup;
 using DFWin.Core.Updaters;
 using FluentAssertions;
 using NUnit.Framework;
@@ -31,14 +35,58 @@ namespace DFWin.Core.Tests
             }
         }
 
+        [Test]
+        public void AllDwarfFortressStates_HaveCorrespondingDwarfFortressInputs()
+        {
+            var inputs = GetDwarfFortressInputs().ToList();
+            foreach (var state in GetDwarfFortressStates())
+            {
+                var nameOfInput = state.Name.Substring(0, state.Name.Length - "State".Length) + "Input";
+                inputs.Should().Contain(s => s.Name == nameOfInput, $"The DF state {state.Name} should have a corresponding DF input.");
+            }
+        }
+
+        [Test]
+        public void AllDwarfFortressStates_CanBeCreatedDirectlyFromTheirDwarfFortressInput()
+        {
+            foreach (var state in GetDwarfFortressStates())
+            {
+                if (state.BaseType == null) throw new InvalidOperationException();
+
+                var typeOfInput = FindImplementation(state.BaseType.GetGenericArguments()[0]);
+                var fakeInput = (IDwarfFortressInput) BogusHelpers.Fake(typeOfInput)();
+                var dfState = StateHelpers.CreateInitialScreenState(state.Name, fakeInput);
+                dfState.Should().NotBeNull($"The DF state {state.Name} should be possible to create given just the corresponding DF input.");
+            }
+        }
+
         private IEnumerable<Type> GetUpdaters()
         {
             return coreAssembly.ExportedTypes.Where(t => typeof(IUpdater).IsAssignableFrom(t) && !t.IsAbstract);
         }
 
+        private IEnumerable<Type> GetDwarfFortressStates()
+        {
+            return GetStates().Where(t =>
+                t.BaseType != null &&
+                t.BaseType.IsGenericType &&
+                t.BaseType.GetGenericTypeDefinition() == typeof(DwarfFortressState<>));
+        }
+
         private IEnumerable<Type> GetStates()
         {
             return coreAssembly.ExportedTypes.Where(t => typeof(IScreenState).IsAssignableFrom(t) && !t.IsAbstract);
+        }
+
+        private IEnumerable<Type> GetDwarfFortressInputs()
+        {
+            return coreAssembly.ExportedTypes.Where(t => typeof(IDwarfFortressInput).IsAssignableFrom(t) && !t.IsAbstract);
+        }
+
+        private Type FindImplementation(Type type)
+        {
+            if (!type.IsAbstract) return type;
+            return coreAssembly.ExportedTypes.Single(t => !t.IsAbstract && type.IsAssignableFrom(t));
         }
     }
 }
---
+++ DFWin/DFWin.Core.Tests/TestSetup/BogusHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using Bogus;
+
+namespace DFWin.Core.Tests.TestSetup
+{
+    public static class BogusHelpers
+    {
+        public static Func<object> Fake(Type concreteTypeToFake)
+        {
+            var method = typeof(BogusHelpers).GetMethod(nameof(FakeGeneric), BindingFlags.NonPublic | BindingFlags.Static);
+            var genericMethod = method.MakeGenericMethod(concreteTypeToFake);
+            return (Func<object>)genericMethod.Invoke(null, null);
+        }
+
+        private static Func<object> FakeGeneric<T>()
+            where T : class
+        {
+            var faker = new Faker<T>();
+            return () => faker.Generate();
+        }
+    }
+}
