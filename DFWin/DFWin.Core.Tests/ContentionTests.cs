48bb02132bbf02657d27473a7b2a094cf5540b11
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-06-02 11:50:24 +0200

    Adding first tests + fixing target architecture

---
+++ DFWin/DFWin.Core.Tests/ContentionTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DFWin.Core.States;
+using DFWin.Core.Updaters;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DFWin.Core.Tests
+{
+    [TestFixture]
+    public class ConventionTests
+    {
+        private Assembly coreAssembly;
+
+        [SetUp]
+        public void SetUp()
+        {
+            coreAssembly = typeof(CoreModule).Assembly;
+        }
+
+        [Test]
+        public void AllScreenStates_HaveCorrespondingUpdaters()
+        {
+            var screens = GetUpdaters().ToList();
+            foreach (var state in GetStates())
+            {
+                var nameOfUpdater = state.Name.Substring(0, state.Name.Length - "State".Length) + "Updater";
+                screens.Should().Contain(s => s.Name == nameOfUpdater, $"The state {state.Name} should have a corresponding updater.");
+            }
+        }
+
+        private IEnumerable<Type> GetUpdaters()
+        {
+            return coreAssembly.ExportedTypes.Where(t => typeof(IUpdater).IsAssignableFrom(t) && !t.IsAbstract);
+        }
+
+        private IEnumerable<Type> GetStates()
+        {
+            return coreAssembly.ExportedTypes.Where(t => typeof(IScreenState).IsAssignableFrom(t) && !t.IsAbstract);
+        }
+    }
+}
---
+++ DFWin/DFWin.Tests/ConventionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DFWin.Core;
+using DFWin.Core.Models;
+using DFWin.Core.States;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DFWin.Tests
+{
+    [TestFixture]
+    public class ConventionTests
+    {
+        private Assembly winAssembly;
+        private Assembly coreAssembly;
+
+        [SetUp]
+        public void SetUp()
+        {
+            winAssembly = typeof(GameModule).Assembly;
+            coreAssembly = typeof(CoreModule).Assembly;
+        }
+
+        [Test]
+        public void AllScreenStates_HaveCorrespondingScreens()
+        {
+            var screens = GetScreens().ToList();
+            foreach (var state in GetStates())
+            {
+                var nameOfScreen = state.Name.Substring(0, state.Name.Length - "State".Length) + "Screen";
+                screens.Should().Contain(s => s.Name == nameOfScreen, $"The state {state.Name} should have a corresponding screen.");
+            }
+        }
+
+        private IEnumerable<Type> GetScreens()
+        {
+            return winAssembly.ExportedTypes.Where(t => typeof(IScreen).IsAssignableFrom(t) && !t.IsAbstract);
+        }
+
+        private IEnumerable<Type> GetStates()
+        {
+            return coreAssembly.ExportedTypes.Where(t => typeof(IScreenState).IsAssignableFrom(t) && !t.IsAbstract);
+        }
+    }
+}
